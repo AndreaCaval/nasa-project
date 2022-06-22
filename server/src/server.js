6230258286083aaa336f8611b0e6f89fd372cfbe
AndreaCaval <andrea.cavalluzzo2000@gmail.com>
2022-06-22 23:29:15 +0200

    section 9 completed

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const { loadPlanetsData } = require('./models/planets.module');
+const { loadPlanetsData } = require('./models/planets.model');
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
